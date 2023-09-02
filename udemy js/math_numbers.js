f6c9c56c7868e6f549d689676324357cb2c8c77f
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-09-02 22:23:03 +0500

    math_numbers.js

---
+++ udemy js/math_numbers.js
@@ -0,0 +1,21 @@
+const dollor = 500;
+const dollor2 = 9000;
+let val;
+
+// Simple math with numbers
+val = dollor1 + dollor2;
+val = dollor1 - dollor2;
+val = dollor1 * dollor2;
+val = dollor1 / dollor2;
+val = dollor1 % dollor2;
+
+
+
+// Math objecty;
+val = Math.PI;
+val = Math.E;
+val = Math.round(444,66);
+val = Math.sqrt(81);
+
+
+
