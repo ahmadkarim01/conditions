f13aa85921d1f97ff16346cc0770746d87933cb0
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-09-02 22:20:56 +0500

    app.js

---
+++ udemy js/app.js
@@ -0,0 +1,12 @@
+// const name = "Ahmad Karim";
+// console.log(nametype);
+
+//  show you different datatypes in javascript:
+
+// string
+const name = "Ahmad Karim"
+
+console.log(typeof name)
+
+//
+
